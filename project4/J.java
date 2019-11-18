1e4ac26239f1af1313f070bcb2ebeca60b07d2db
wjinlin <983527201@qq.com>
2019-11-18 20:42:48 +0800

    java啦

---
+++ project4/J.java
@@ -0,0 +1,39 @@
+package wangzong;
+
+import java.util.Scanner;
+
+public class J{
+	public static void main(String []args){
+		//根据年龄, 来打印出当前年龄的人是少年(低于18), 青年(19-28), 中年(29-55), 老年(56以上)
+		/** int a=55;
+		if(a>0&&a<=18) {
+			System.out.println("这娃是少年");
+		}
+		else if(a>=19&&a<=28) {
+			System.out.println("这娃是青年");
+		}
+		else if(a>=29&&a<=55) {
+			System.out.println("这娃是中年");
+		}
+		else{
+			System.out.println("这娃是老年");
+		}*/
+		//判定一个数字是否是素数
+		/**Scanner a =new Scanner(System.in);
+		System.out.println("输入判断数：");
+		int b=a.nextInt();
+		 for(int i=2;i<=b;i++) {
+			if(b%i == 0&& i !=b) {
+			System.out.println("不是素数！");
+			 break;
+			}
+		    else if(i == b){
+		    System.out.println("是素数！");
+		    } 
+	     }
+	     */
+		//打印1--100之间的素数
+		
+	}	
+	
+}
